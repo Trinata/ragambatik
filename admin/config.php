6f30ccdea96ecd78e4c830cd7c8c53d697316138
Ovan Pulu <ovan89@gmail.com>
2014-12-04 18:45:21 +0700

    add original file

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ragambatik/admin/');
+define('HTTP_CATALOG', 'http://localhost/ragambatik/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ragambatik/admin/');
+define('HTTPS_CATALOG', 'http://localhost/ragambatik/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ovancop/Data/htdocs/ragambatik/admin/');
+define('DIR_SYSTEM', '/home/ovancop/Data/htdocs/ragambatik/system/');
+define('DIR_DATABASE', '/home/ovancop/Data/htdocs/ragambatik/system/database/');
+define('DIR_LANGUAGE', '/home/ovancop/Data/htdocs/ragambatik/admin/language/');
+define('DIR_TEMPLATE', '/home/ovancop/Data/htdocs/ragambatik/admin/view/template/');
+define('DIR_CONFIG', '/home/ovancop/Data/htdocs/ragambatik/system/config/');
+define('DIR_IMAGE', '/home/ovancop/Data/htdocs/ragambatik/image/');
+define('DIR_CACHE', '/home/ovancop/Data/htdocs/ragambatik/system/cache/');
+define('DIR_DOWNLOAD', '/home/ovancop/Data/htdocs/ragambatik/download/');
+define('DIR_LOGS', '/home/ovancop/Data/htdocs/ragambatik/system/logs/');
+define('DIR_CATALOG', '/home/ovancop/Data/htdocs/ragambatik/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root123root');
+define('DB_DATABASE', 'db_ragam_batik');
+define('DB_PREFIX', 'batik_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ragambatik/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ragambatik/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ovancop/Data/htdocs/ragambatik/catalog/');
+define('DIR_SYSTEM', '/home/ovancop/Data/htdocs/ragambatik/system/');
+define('DIR_DATABASE', '/home/ovancop/Data/htdocs/ragambatik/system/database/');
+define('DIR_LANGUAGE', '/home/ovancop/Data/htdocs/ragambatik/catalog/language/');
+define('DIR_TEMPLATE', '/home/ovancop/Data/htdocs/ragambatik/catalog/view/theme/');
+define('DIR_CONFIG', '/home/ovancop/Data/htdocs/ragambatik/system/config/');
+define('DIR_IMAGE', '/home/ovancop/Data/htdocs/ragambatik/image/');
+define('DIR_CACHE', '/home/ovancop/Data/htdocs/ragambatik/system/cache/');
+define('DIR_DOWNLOAD', '/home/ovancop/Data/htdocs/ragambatik/download/');
+define('DIR_LOGS', '/home/ovancop/Data/htdocs/ragambatik/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root123root');
+define('DB_DATABASE', 'db_ragam_batik');
+define('DB_PREFIX', 'batik_');
+?>
